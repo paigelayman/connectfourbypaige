4cf9cd36dc9c548e719d3cfd615f36fbd235ccfd
Paige Layman <paigelayman96@gmail.com>
2022-10-04 11:15:05 -0400

    made alternating turns and disabled after click

--- app.js
+++ app.js
@@ -3,9 +3,36 @@ let theirScore = 0
 let compScore = document.querySelector('.comp-score')
 let playerScore = document.querySelector('.player-score')
 let gameOver = document.querySelector('.game-over')
+let gameBoard = document.querySelector('.board')
 let spaces = document.getElementsByClassName('space')
 
-for (const space of spaces)
-  space.addEventListener('click', () => {
-    space.style.backgroundColor = '#b091c9'
-  })
+function playerTurn() {
+  let boardClicks = 0
+  for (const space of spaces)
+    space.addEventListener('click', () => {
+      boardClicks += 1
+
+      if (boardClicks % 2 == 0) {
+        for (const space of spaces)
+          space.addEventListener('click', () => {
+            space.style.backgroundColor = '#b091c9'
+            space.classList.add('disable')
+          })
+      } else {
+        for (const space of spaces)
+          space.addEventListener('click', () => {
+            space.style.backgroundColor = '#1c012b'
+            space.classList.add('disable')
+          })
+      }
+    })
+}
+playerTurn()
+
+//disable button after click
+
+//make it so the bottom row has to fill first
+
+//add winning combinations
+
+//add to scoreboard
