28967d8172fe7e1bf8c372cb983685947ad6dbed
Paige Layman <paigelayman96@gmail.com>
2022-10-04 12:02:01 -0400

    added function to play again button

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ let playerScore = document.querySelector('.player-score')
 let gameOver = document.querySelector('.game-over')
 let gameBoard = document.querySelector('.board')
 let spaces = document.getElementsByClassName('space')
+let spaceNumber = document.getElementById('space')
+let replay = document.querySelector('.play-again')
 
 function playerTurn() {
   let boardClicks = 0
   for (const space of spaces)
     space.addEventListener('click', () => {
       boardClicks += 1
-
       if (boardClicks % 2 == 0) {
         for (const space of spaces)
           space.addEventListener('click', () => {
@@ -29,10 +30,18 @@ function playerTurn() {
 }
 playerTurn()
 
-//disable button after click
-
 //make it so the bottom row has to fill first
 
 //add winning combinations
 
 //add to scoreboard
+
+//play again button
+function playAgain() {
+  for (const space of spaces)
+    replay.addEventListener('click', () => {
+      space.style.backgroundColor = '#ebe4f2'
+      space.classList.remove('disable')
+    })
+}
+playAgain()
