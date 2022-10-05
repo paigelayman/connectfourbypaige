b6ae949da8ee44742855f87a46aaa35aecac4e57
Paige Layman <paigelayman96@gmail.com>
2022-10-05 11:48:22 -0400

    winning conditions will console log which player won

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
 let oneScore = 0
 let twoScore = 0
-
 let playerOneScore = document.querySelector('.player-one-score')
 let playerTwoScore = document.querySelector('.player-two-score')
 let gameOver = document.querySelector('.game-over')
 let gameBoard = document.querySelector('.board')
 let spaces = document.getElementsByClassName('space')
-let spaceNumber = document.getElementById('space')
 let replay = document.querySelector('.play-again')
+let newGame = [
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  ''
+]
 let winningCombos = [
   [0, 1, 2, 3],
   [1, 2, 3, 4],
@@ -61,59 +96,72 @@ let winningCombos = [
   [3, 11, 19, 27]
 ]
 let boardClicks = 0
-function playerTurn() {
+let playerTurn = () => {
   for (const space of spaces)
     space.addEventListener('click', () => {
       boardClicks += 1
       if (boardClicks % 2 == 1) {
         for (const space of spaces)
           space.addEventListener('click', () => {
+            newGame[parseInt(space.id)] = 'x'
             space.style.backgroundColor = '#b091c9'
-            space.classList.add('disable')
+            space.innerText = 'x'
+            space.classList.add('disable', 'x')
+            roundWon()
           })
       } else {
         for (const space of spaces)
           space.addEventListener('click', () => {
+            newGame[parseInt(space.id)] = 'o'
             space.style.backgroundColor = '#d97c7c'
-            space.classList.add('disable')
+            space.innerText = 'o'
+            space.classList.add('disable', 'o')
+            roundWon()
           })
       }
     })
 }
 playerTurn()
 
-function roundWon() {
-  let wonRound = false
+let roundWon = () => {
   for (let i = 0; i < winningCombos.length; i++) {
     let winner = winningCombos[i]
-    let win1 = spaces[winner[i]][0]
-    let win2 = spaces[winner[i]][1]
-    let win3 = spaces[winner[i]][2]
-    let win4 = spaces[winner[i]][3]
-    if (win1 == '' || win2 == '' || win3 == '' || win4 == '') {
+    let win1 = newGame[winner[0]]
+    let win2 = newGame[winner[1]]
+    let win3 = newGame[winner[2]]
+    let win4 = newGame[winner[3]]
+    if (win1 === '' || win2 === '' || win3 === '' || win4 === '') {
       continue
     }
-    if (win1 == win2 && win2 == win3 && win3 == win4 && boardClicks % 2 === 1) {
+    if (
+      win1 === win2 &&
+      win2 === win3 &&
+      win3 === win4 &&
+      boardClicks % 2 === 0
+    ) {
       console.log('player 1 wins')
-      playerOneScore += 1
-      playerOneScore.innertext = playerOneScore
-      wonRound = true
+      oneScore = oneScore + 1
+      playerOneScore.innertext = `${oneScore}`
       break
     }
-    if (win1 == win2 && win2 == win3 && win3 == win4 && boardClicks % 2 === 1) {
+    if (
+      win1 === win2 &&
+      win2 === win3 &&
+      win3 === win4 &&
+      boardClicks % 2 === 1
+    ) {
       console.log('player 2 wins')
-      playerTwoScore = playerTwoScore + 1
-      playerTwoScore.innertext = `${playerTwoScore}`
-      wonRound = true
+      twoScore += 1
+      playerTwoScore.innertext = twoScore
       break
     }
   }
 }
-roundWon()
+
 //add above scoreboard player_ won play again?
 //add message
 
-function playAgain() {
+let playAgain = () => {
   for (const space of spaces)
     replay.addEventListener('click', () => {
       space.style.backgroundColor = '#ebe4f2'
@@ -122,7 +170,9 @@ function playAgain() {
       playerTwoScore = 0
     })
 }
-playAgain()
+// playAgain()
 
 //make it so the bottom row has to fill first
 // get welcome page to open first
+// console.log(oneScore)
+// console.log(twoScore)
